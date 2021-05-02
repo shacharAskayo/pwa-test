254663b7bdad3b97d53070b240b0cbc690b775f6
Shachar Askayo <askayo122@gmail.com>
2021-05-02 17:22:08 +0300

    adding console.log

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('fuckkkk the he');
   return (
     <div className="App">
       <header className="App-header">
